4286b626dcd6ba0035d8055512419fe5cec2bae5
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2025-12-18 23:56:45 +0700

    update

--- prisma/migrations/20251218153247_enable_realtime_quiz_attempt/migration.sql
+++ prisma/migrations/20251218153247_enable_realtime_quiz_attempt/migration.sql
@@ -1,2 +1,8 @@
--- Enable Realtime for QuizAttempt
-alter publication supabase_realtime add table "QuizAttempt";
+-- Enable Realtime for QuizAttempt (Safe for Shadow DB)
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_publication WHERE pubname = 'supabase_realtime') THEN
+    ALTER PUBLICATION supabase_realtime ADD TABLE "QuizAttempt";
+  END IF;
+END
+$$;
